58799d18ce34f85b32a120e5eda99fb5e3a5e595
Go Saito <gos@iij.ad.jp>
2017-01-13 15:34:24 +0900

    remove unused struct

--- t/util.lisp
+++ t/util.lisp
@@ -8,13 +8,9 @@
                 :ofp_header-version))
 (in-package :kappa-test.util)
 
-(plan nil)
+(plan 1)
 
 (subtest "with-prefix"
-  (defstruct hoge
-    foo
-    bar)
-
   (let ((s (make-ofp_header :version 1)))
     (with-prefix (h ofp_header-)
       (is (h version s) 1))))
